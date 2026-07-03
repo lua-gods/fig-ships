01a000f49bb2f5308879c718e8f9dd23b629ebe8
GN68 <81744903+GN68@users.noreply.github.com>
2026-07-04 03:35:06 +0800

    fixed edge case with non minecraft blocks in paint blocks

--- lib/Ship.lua
+++ lib/Ship.lua
@@ -124,7 +124,7 @@ local SHIP_PARTS = {}
 local PAINT_BLOCKS = {}
 for index, id in ipairs(client.getRegistry("minecraft:block")) do
 	local id = id:match("minecraft:(.*)")
-	if id:find("concrete") or id:find("wool") or id:find("teracotta") or id:find("glazed") then
+	if id and id:find("concrete") or id:find("wool") or id:find("teracotta") or id:find("glazed") then
 		local block = world.newBlock(id)
 		if block:isOpaque() and block:isSolidBlock() then
 			PAINT_BLOCKS[#PAINT_BLOCKS + 1] = id
